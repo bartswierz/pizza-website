03631ad3c7f4c2957127ef0bcbbd92256eb7f044
bartswierz <bartoszswierzynski7@gmail.com>
2022-08-23 07:55:37 +0000

    added wing logic

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ Add the above code to all click events to PREVENT FORM SUBMISSION, This will all
 
 //TODO: Move wings, sides, desserts to front page
 //TODO: Change HTML 'starting at $12.99' to show the correct price
+//TODO: Fix total price to 2 decimal points
 
-// PIZZA FORMS
+// ALL FORMS
 const pepperoniPizzaForm = document.getElementById("pepperoniPizzaForm");
 const sausagePizzaForm = document.getElementById("sausagePizzaForm");
 const cheesePizzaForm = document.getElementById("cheesePizzaForm");
@@ -16,8 +17,14 @@ const hawaiianPizzaForm = document.getElementById("hawaiianPizzaForm");
 const buffaloChickenPizzaForm = document.getElementById(
   "buffaloChickenPizzaForm"
 );
+const bbqWingForm = document.getElementById("bbqWingForm");
+const buffaloWingForm = document.getElementById("buffaloWingForm");
+// const mildWingForm = document.getElementById("mildWingForm");
+// const koreanBbqWingForm = document.getElementById("koreanBbqWingForm");
+// const teriyakiWingForm = document.getElementById("teriyakiWingForm");
+// const buffaloWingForm = document.getElementById("buffaloWingForm");
 
-// PIZZA BUTTONS
+// ALL BUTTONS
 const btnPepperoniPizza = document.querySelector(".form__btn--pepperoni-pizza");
 const btnSausagePizza = document.querySelector(".form__btn--sausage-pizza");
 const btnCheesePizza = document.querySelector(".form__btn--cheese-pizza");
@@ -26,15 +33,30 @@ const btnHawaiianPizza = document.querySelector(".form__btn--hawaiian-pizza");
 const btnBuffaloChickenPizza = document.querySelector(
   ".form__btn--buffalo-chicken-pizza"
 );
+const btnBbqWings = document.querySelector(".form__btn--bbq-wings");
+const btnBuffaloWings = document.querySelector(".form__btn--buffalo-wings");
+// const btnMildWings = document.querySelector(".form__btn--mild-wings");
+// const btnKoreanBbqWings = document.querySelector(".form__btn--korean-bbq-wings");
+// const btnBuffaloWings = document.querySelector(".form__btn--buffalo-wings");
+// const btnBuffaloWings = document.querySelector(".form__btn--buffalo-wings");
 
-// HOLDS ALL ORDER DETAILS
+// id="bbq_wing"
+//id="bbq_quantity"
+
+// ORDERS OBJECT
 const orders = {
   pizzaOrders: [],
+  wingOrders: [],
+  // sidesOrders: [],
+  // dessertsOrders: [],
 };
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-// HELPER FUNCTIONS
-
+/* 
+  ==========================================================
+                      PIZZA LOGIC
+  ==========================================================
+*/
+//  PIZZA HELPER FUNCTIONS
 // Calculates pizza price based on chosen Pizza Size
 const calcPrice = function (
   userSize,
@@ -51,7 +73,7 @@ const calcTotalPrice = function (price, userQuantity) {
   return price * userQuantity;
 };
 
-// Checks if user chose a crust, size, and quantity. "" prevents no empty order attempts
+// Checks if user chose a crust, size, and quantity. "" conditional ensures no option is left unchanged
 const isOrderValid = function (userCrust, userSize, userQuantity) {
   if (
     userCrust !== "Select Crust" &&
@@ -65,8 +87,6 @@ const isOrderValid = function (userCrust, userSize, userQuantity) {
   else return false;
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////
-// PIZZA LOGIC
 // Customer added 'Pepperoni Pizza' to their order
 btnPepperoniPizza.addEventListener("click", function (event) {
   // PREVENTS FORM SUBMISSION
@@ -237,5 +257,86 @@ btnBuffaloChickenPizza.addEventListener("click", function (event) {
   } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
 });
 
-/////////////////////////////////////////////////////////////////////////////////////////
-// WINGS LOGIC
+/* 
+==========================================================
+                    WING LOGIC
+==========================================================
+*/
+
+// WINGS HELPER FUNCTIONS
+// Calculates Wing Price based on quantity selected
+const calcWingPrice = function (
+  userQuantity,
+  [priceOf_8, priceOf_16, priceOf_24, priceOf_48]
+) {
+  if (userQuantity === "8") return priceOf_8;
+  if (userQuantity === "16") return priceOf_16;
+  if (userQuantity === "24") return priceOf_24;
+  if (userQuantity === "48") return priceOf_48;
+  else return;
+};
+
+// Checks if user chose a wing style and quantity. "" conditional ensures no option is left unchanged
+const isWingOrderValid = function (userWing, userQuantity) {
+  if (
+    userWing !== "Select Wing Style" &&
+    userWing !== "" &&
+    userQuantity !== "Quantity" &&
+    userQuantity !== ""
+  )
+    return true;
+  else return false;
+};
+//////////////////////////////////////////////////////
+btnBbqWings.addEventListener("click", function (event) {
+  // PREVENTS FORM SUBMISSION
+  event.preventDefault();
+
+  let userChoice = []; //Will hold our five items
+  // const userCrust = pepperoniPizzaForm.pepperoni_crust.value;
+  const userWing = bbqWingForm.bbq_wing.value;
+  const userQuantity = bbqWingForm.bbq_quantity.value;
+
+  // If user input is valid(User chose BOTH options)
+  if (isWingOrderValid(userWing, userQuantity)) {
+    console.log(
+      `Order received! ${userQuantity} ${userWing} honey bbq wings have been ordered.`
+    ); //Add this
+
+    // Calculates price of item and total price
+    let price = calcWingPrice(userQuantity, [7.99, 15.99, 23.99, 47.99]);
+
+    //Pushing order details into Object 'orders'
+    userChoice = [[userWing, userQuantity, price, "Honey BBQ"]]; //sending array to keep pizza orders separate each push
+    orders.wingOrders.push(userChoice);
+    console.log(orders.wingOrders);
+    console.log(orders.wingOrders.length);
+  } else alert("Incomplete order, please choose a wing style and quantity. Thanks!");
+});
+
+//BUFFALO WINGS
+btnBuffaloWings.addEventListener("click", function (event) {
+  // PREVENTS FORM SUBMISSION
+  event.preventDefault();
+
+  let userChoice = []; //Will hold our five items
+  // const userCrust = pepperoniPizzaForm.pepperoni_crust.value;
+  const userWing = buffaloWingForm.buffalo_wing.value;
+  const userQuantity = buffaloWingForm.buffalo_quantity.value;
+
+  // If user input is valid(User chose BOTH options)
+  if (isWingOrderValid(userWing, userQuantity)) {
+    console.log(
+      `Order received! ${userQuantity} ${userWing} buffalo wings have been ordered.`
+    ); //Add this
+
+    // Calculates price of item and total price
+    let price = calcWingPrice(userQuantity, [7.99, 15.99, 23.99, 47.99]);
+
+    //Pushing order details into Object 'orders'
+    userChoice = [[userWing, userQuantity, price, "Buffalo"]]; //sending array to keep pizza orders separate each push
+    orders.wingOrders.push(userChoice);
+    console.log(orders.wingOrders);
+    console.log(orders.wingOrders.length);
+  } else alert("Incomplete order, please choose a wing style and quantity. Thanks!");
+});
