f1045fcd997fe04dd7a4f0c32ec03f1a94cf0863
bartswierz <bartoszswierzynski7@gmail.com>
2022-08-23 04:42:24 +0000

    added buttons and updated HTML Form IDs for all 6 pizzas

--- script.js
+++ script.js
@@ -27,16 +27,26 @@ const empty = "";
 // PIZZA FORMS
 const pepperoniPizzaForm = document.getElementById("pepperoniPizzaForm");
 const sausagePizzaForm = document.getElementById("sausagePizzaForm");
+const cheesePizzaForm = document.getElementById("cheesePizzaForm");
+const veggiePizzaForm = document.getElementById("veggiePizzaForm");
+const hawaiianPizzaForm = document.getElementById("hawaiianPizzaForm");
+const buffaloChickenPizzaForm = document.getElementById(
+  "buffaloChickenPizzaForm"
+);
 
 // PIZZA BUTTONS
 const btnPepperoniPizza = document.querySelector(".form__btn--pepperoni-pizza");
 const btnSausagePizza = document.querySelector(".form__btn--sausage-pizza");
+const btnCheesePizza = document.querySelector(".form__btn--cheese-pizza");
+const btnVeggiePizza = document.querySelector(".form__btn--veggie-pizza");
+const btnHawaiianPizza = document.querySelector(".form__btn--hawaiian-pizza");
+const btnBuffaloChickenPizza = document.querySelector(
+  ".form__btn--buffalo-chicken-pizza"
+);
 
+// HOLDS ALL ORDER DETAILS
 const orders = {
-  // Will hold our: crust, size, quantity, price, totalPrice
-  currentPizzaOrders: [],
-  // price: 0,
-  // totalPrice: 0
+  pizzaOrders: [],
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -98,13 +108,13 @@ btnPepperoniPizza.addEventListener("click", function (event) {
     userChoice = [
       [userCrust, userSize, userQuantity, price, totalPrice, "Pepperoni"],
     ]; //sending array to keep pizza orders separate each push
-    orders.currentPizzaOrders.push(userChoice);
-    // console.log(orders.currentPizzaOrders);
-    console.log(orders.currentPizzaOrders.length);
+    orders.pizzaOrders.push(userChoice);
+    console.log(orders.pizzaOrders);
+    console.log(orders.pizzaOrders.length);
   } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
 });
 
-//Pizza #2 Sausage
+// SAUSAGE PIZZA
 btnSausagePizza.addEventListener("click", function (event) {
   // PREVENTS FORM SUBMISSION
   event.preventDefault();
@@ -118,18 +128,128 @@ btnSausagePizza.addEventListener("click", function (event) {
   if (isOrderValid(userCrust, userSize, userQuantity, "")) {
     console.log(
       `Order Received! ${userQuantity} ${userSize} ${userCrust} sausage pizza(s) have been ordered.`
-    ); //Add this
+    );
 
     // Calculates price of item and total price
     price = calcPrice(userSize, [12.99, 15.99, 18.99]);
     totalPrice = calcTotalPrice(price, userQuantity);
 
-    //Pushing order details into Object 'orders'
+    //Placing ALL order details into an array and pushing into Object 'orders'
     userChoice = [
       [userCrust, userSize, userQuantity, price, totalPrice, "Sausage"],
-    ]; //sending array to keep pizza orders separate each push
-    orders.currentPizzaOrders.push(userChoice);
-    // console.log(orders.currentPizzaOrders);
-    console.log(orders.currentPizzaOrders.length);
+    ];
+    orders.pizzaOrders.push(userChoice);
+  } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
+});
+
+// CHEESE PIZZA
+btnCheesePizza.addEventListener("click", function (event) {
+  // PREVENTS FORM SUBMISSION
+  event.preventDefault();
+
+  let userChoice = []; //Will hold our five items
+  const userCrust = cheesePizzaForm.cheese_crust.value;
+  const userSize = cheesePizzaForm.cheese_size.value;
+  const userQuantity = cheesePizzaForm.cheese_quantity.value;
+
+  // If user input is valid, then PUSH order details to object 'orders'
+  if (isOrderValid(userCrust, userSize, userQuantity, "")) {
+    console.log(
+      `Order Received! ${userQuantity} ${userSize} ${userCrust} cheese pizza(s) have been ordered.`
+    );
+
+    // Calculates price of item and total price
+    price = calcPrice(userSize, [12.99, 15.99, 18.99]);
+    totalPrice = calcTotalPrice(price, userQuantity);
+
+    //Placing ALL order details into an array and pushing into Object 'orders'
+    userChoice = [
+      [userCrust, userSize, userQuantity, price, totalPrice, "Cheese"],
+    ];
+    orders.pizzaOrders.push(userChoice);
+  } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
+});
+
+// VEGGIE PIZZA
+btnVeggiePizza.addEventListener("click", function (event) {
+  // PREVENTS FORM SUBMISSION
+  event.preventDefault();
+
+  let userChoice = []; //Will hold our five items
+  const userCrust = veggiePizzaForm.veggie_crust.value;
+  const userSize = veggiePizzaForm.veggie_size.value;
+  const userQuantity = veggiePizzaForm.veggie_quantity.value;
+
+  // If user input is valid, then PUSH order details to object 'orders'
+  if (isOrderValid(userCrust, userSize, userQuantity, "")) {
+    console.log(
+      `Order Received! ${userQuantity} ${userSize} ${userCrust} veggie pizza(s) have been ordered.`
+    );
+
+    // Calculates price of item and total price
+    price = calcPrice(userSize, [12.99, 15.99, 18.99]);
+    totalPrice = calcTotalPrice(price, userQuantity);
+
+    //Placing ALL order details into an array and pushing into Object 'orders'
+    userChoice = [
+      [userCrust, userSize, userQuantity, price, totalPrice, "Veggie"],
+    ];
+    orders.pizzaOrders.push(userChoice);
+  } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
+});
+
+//Hawaiian Pizza
+btnHawaiianPizza.addEventListener("click", function (event) {
+  // PREVENTS FORM SUBMISSION
+  event.preventDefault();
+
+  let userChoice = []; //Will hold our five items
+  const userCrust = hawaiianPizzaForm.hawaiian_crust.value;
+  const userSize = hawaiianPizzaForm.hawaiian_size.value;
+  const userQuantity = hawaiianPizzaForm.hawaiian_quantity.value;
+
+  // If user input is valid, then PUSH order details to object 'orders'
+  if (isOrderValid(userCrust, userSize, userQuantity, "")) {
+    console.log(
+      `Order Received! ${userQuantity} ${userSize} ${userCrust} hawaiian pizza(s) have been ordered.`
+    );
+
+    // Calculates price of item and total price
+    price = calcPrice(userSize, [12.99, 15.99, 18.99]);
+    totalPrice = calcTotalPrice(price, userQuantity);
+
+    //Placing ALL order details into an array and pushing into Object 'orders'
+    userChoice = [
+      [userCrust, userSize, userQuantity, price, totalPrice, "Hawaiian"],
+    ];
+    orders.pizzaOrders.push(userChoice);
+  } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
+});
+
+// BUFFALO CHICKEN PIZZA
+btnBuffaloChickenPizza.addEventListener("click", function (event) {
+  // PREVENTS FORM SUBMISSION
+  event.preventDefault();
+
+  let userChoice = []; //Will hold our five items
+  const userCrust = buffaloChickenPizzaForm.buffalo_chicken_crust.value;
+  const userSize = buffaloChickenPizzaForm.buffalo_chicken_size.value;
+  const userQuantity = buffaloChickenPizzaForm.buffalo_chicken_quantity.value;
+
+  // If user input is valid, then PUSH order details to object 'orders'
+  if (isOrderValid(userCrust, userSize, userQuantity, "")) {
+    console.log(
+      `Order Received! ${userQuantity} ${userSize} ${userCrust} buffalo chicken pizza(s) have been ordered.`
+    );
+
+    // Calculates price of item and total price
+    price = calcPrice(userSize, [12.99, 15.99, 18.99]);
+    totalPrice = calcTotalPrice(price, userQuantity);
+
+    //Placing ALL order details into an array and pushing into Object 'orders'
+    userChoice = [
+      [userCrust, userSize, userQuantity, price, totalPrice, "Buffalo Chicken"],
+    ];
+    orders.pizzaOrders.push(userChoice);
   } else alert("Incomplete order, please choose a crust, size, and quantity. Thanks!");
 });
